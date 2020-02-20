059f2952f287debc8efd96cf7dae2838e373315c
minchulahn <minchulahn@inslab.co.kr>
2020-02-20 14:05:21 +0900

    prod environment 설정

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
+import { KeycloakOptions } from 'keycloak-angular';
+
+const keycloakOptions: KeycloakOptions = {
+  config: {
+    url: 'https://keycloak.gantry.ai/auth',
+    realm: 'gantry',
+    clientId: 'dashboard'
+  },
+  initOptions: {
+    onLoad: 'login-required',
+    checkLoginIframe: false
+  },
+  enableBearerInterceptor: true,
+  bearerExcludedUrls: ['/assets', '/clients/public']
+}
+
 export const environment = {
-  production: true
+  production: true,
+  keycloakOptions: keycloakOptions
 };
