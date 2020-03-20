cf21f97c071d73420714363be3e8a4719f53b2f1
minchulahn <minchulahn@inslab.co.kr>
2020-03-20 12:28:02 +0900

    local environment 추가

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,21 @@
+import { KeycloakOptions } from 'keycloak-angular';
+
+const keycloakOptions: KeycloakOptions = {
+  config: {
+    url: 'https://keycloak.gantry.ai/auth',
+    realm: 'gantry',
+    clientId: 'dashboard'
+  },
+  initOptions: {
+    onLoad: 'login-required',
+    checkLoginIframe: false
+  },
+  enableBearerInterceptor: true,
+  bearerExcludedUrls: ['/assets']
+}
+
+export const environment = {
+  production: false,
+  keycloakOptions: keycloakOptions,
+  userManagementRestApi: 'http://localhost:8080'
+};
