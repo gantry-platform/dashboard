309b25af4c82d3e7c1d59ee345239b055e65a9fb
minchulahn <minchulahn@inslab.co.kr>
2020-08-10 12:14:27 +0900

    화면 스타일 수정

--- src/app/components/project-pipeline-create/project-pipeline-create.component.ts
+++ src/app/components/project-pipeline-create/project-pipeline-create.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+enum SourceType {
+  git = 'Git',
+  image = 'Docker Image'
+}
 
 @Component({
   selector: 'app-project-pipeline-create',
@@ -7,12 +13,60 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class ProjectPipelineCreateComponent implements OnInit {
 
-  @Input()  data: any;
   @Output() result = new EventEmitter<boolean>();
+  SourceTypeDeclaredInComponent = SourceType;
+  form: FormGroup;
+  sourceTypes: Array<String> = [SourceType.git, SourceType.image];
+  currentSourceType: string;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.form = this.fb.group({
+      pipelineName: ['', Validators.required],
+      sourceType: ['', Validators.required],
+      applicationName: ['', Validators.required]
+    });
+  }
+
+  initGitForm(): void {
+    this.form = this.fb.group({
+      pipelineName: ['', Validators.required],
+      sourceType: [SourceType.git, Validators.required]
+    });
+  }
+
+  initDockerImageForm(): void {
+    this.form = this.fb.group({
+      pipelineName: ['', Validators.required],
+      sourceType: [SourceType.image, Validators.required]
+    });
+  }
+
+  resetForm(): void {
+    this.form = null;
+  }
+
+  selectionChangeSourceType(type: string): void {
+    this.currentSourceType = type;
+    this.resetForm();
+
+    if(this.currentSourceType == SourceType.git) {
+      this.initGitForm();
+
+    } else if(this.currentSourceType == SourceType.image) {
+      this.initDockerImageForm();
+    } else {
+      this.initForm();
+    }
+
+    console.log(this.form.value);
   }
 
   cancel(): void {
